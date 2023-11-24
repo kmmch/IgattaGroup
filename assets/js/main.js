de8788b02c1acf1541cec13710dd494f90538f9f
kmmch <kmmch.tech@gmail.com>
2023-11-24 17:11:13 +0900

    スクロールヘッダー

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,22 @@ const clickHamburgerLink = () => {
 hamburgerLinkArr.forEach((link) => {
     link.addEventListener('click', clickHamburgerLink);
 });
+
+
+
+const HEADER_CLASS_NAME = '.header'
+const HEADER_STICKY = 'header--sticky';
+const positionDisplayHeader = 300;
+window.addEventListener('scroll', function() {
+    const header = document.querySelector(HEADER_CLASS_NAME);
+    const headerHeight = header.offsetHeight; // ヘッダーの高さを取得
+    const scrollY = window.scrollY;
+    
+    if (scrollY >= positionDisplayHeader) {
+        header.classList.add(HEADER_STICKY);
+        document.body.style.marginTop = headerHeight + 'px'; // コンテンツにヘッダーの高さ分の余白を設定
+    } else {
+        header.classList.remove(HEADER_STICKY);
+        document.body.style.marginTop = '0'; // コンテンツの余白をリセット
+    }
+});
