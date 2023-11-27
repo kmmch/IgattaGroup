d63e2aa6dd09404d6cce2fdbb1d94698e881f425
Masaya Kemmochi <82385253+kmmch@users.noreply.github.com>
2023-11-27 08:36:46 +0000

    書き方を変える

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,14 @@
-/*
-const HEADER_CLASS_NAME = '.header'
-const HEADER_STICKY = 'header--sticky';
-const positionDisplayHeader = 300;
-window.addEventListener('scroll', function() {
-    const header = document.querySelector(HEADER_CLASS_NAME);
-    const headerHeight = header.offsetHeight; // ヘッダーの高さを取得
-    const scrollY = window.scrollY;
-    
-    if (scrollY >= positionDisplayHeader) {
-        header.classList.add(HEADER_STICKY);
-        document.body.style.marginTop = headerHeight + 'px'; // コンテンツにヘッダーの高さ分の余白を設定
-    } else {
-        header.classList.remove(HEADER_STICKY);
-        document.body.style.marginTop = '0'; // コンテンツの余白をリセット
-    }
-});
-*/
-
 (function($){
 
     // ハンバーガーボタンクリック時
-    $("#hamburgerBtn").on('click',function(){
+    $("#hamburgerBtn").on('click',() => {
 	    $(this).toggleClass('active');
         $("#hamburgerMenu").toggleClass('panelactive');
         $('body').toggleClass('noScroll');
     });
 
     // ハンバーガーメニューのリンククリック時
-    $("#hamburgerMenu a").on('click',function(){
+    $("#hamburgerMenu a").on('click',() => {
         $("#hamburgerBtn").removeClass('active');
         $("#hamburgerMenu").removeClass('panelactive');
         $('body').removeClass('noScroll');
